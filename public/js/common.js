5ab747a2f626339966014f992f1f45b5faf29987
ugjka <ugis.germanis@gmail.com>
2017-07-17 13:59:52 +0300

    buttons switching stuff

--- public/js/common.js
+++ public/js/common.js
@@ -1,2 +1,40 @@
 // Ugis Germanis
 // My javascript
+
+var music = {
+    selected: "byartist",
+};
+
+var sort = new Event('sort');
+
+document.addEventListener("sort", function(e){
+    console.log("Raw: ", music.selected);
+}, false)
+
+window.addEventListener('WebComponentsReady', function(e) {
+    console.log('components ready');
+    artist.addEventListener("change", function(e){
+        if (artist.active == true){
+            song.active=false;
+            album.active=false;
+            music.selected="byartist";
+            document.dispatchEvent(sort);
+        }
+    }, false);
+    song.addEventListener("change", function(e){
+        if (song.active == true) {
+            artist.active=false;
+            album.active=false;
+            music.selected="bysong";
+            document.dispatchEvent(sort);
+        }
+    }, false);
+    album.addEventListener("change", function(e){
+        if (album.active == true) {
+            artist.active=false;
+            song.active=false;
+            music.selected="byalbum";
+            document.dispatchEvent(sort);    
+        }
+    }, false);
+});
