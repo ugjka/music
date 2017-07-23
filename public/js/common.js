935b5c8fefe7644756c6051be669a9b612bc2c54
ugjka <ugis.germanis@gmail.com>
2017-07-23 18:27:27 +0300

    use event delegation

--- public/js/common.js
+++ public/js/common.js
@@ -17,11 +17,13 @@ var music = {
             $(".song").last().attr('id', "sound"+i);
             $(".song").last().attr('index', i);
             $(".song").last().append(this.playlist[i].Title+ " - "+ this.playlist[i].Artist);
-            document.getElementById("sound"+i).addEventListener('click', function(e){
-                music.current= e.target.getAttribute("index");
-                playSong(music.current);
-            });
         }
+        document.getElementById("playlist").addEventListener('click', function(e){
+                if(e.target && e.target.nodeName == "LI") {
+                    music.current= e.target.getAttribute("index");
+                    playSong(music.current);
+                }
+        });
     },
 };
 
