8568f2e1b37b85cb3b4e39e142ec403e931e181c
ugjka <ugis.germanis@gmail.com>
2017-07-26 21:26:48 +0300

    fix likes on firefox

--- public/js/common.js
+++ public/js/common.js
@@ -43,12 +43,12 @@ window.addEventListener('WebComponentsReady', function(e) {
     document.getElementById("pauseit").addEventListener("click", function(e){
         pause();
     });
-    document.getElementById("favorite").addEventListener("click", function(e){
+    document.getElementById("favoriteit").addEventListener("click", function(e){
         $.get(
             [music.url, "/like"].join(""),
             {"like" : music.playlist[music.current].ID},
             function(data){
-                $("#favorite").attr("favorited", JSON.parse(data));
+                document.getElementById("favoriteit").setAttribute("favorited", JSON.parse(data));
             }
         );
     });
@@ -169,7 +169,7 @@ function playSong(id){
         [music.url, "/like"].join(""),
         {"id" : music.playlist[id].ID},
         function(data){
-            $("#favorite").attr("favorited", JSON.parse(data));
+            document.getElementById("favoriteit").setAttribute("favorited", JSON.parse(data));
         }
     );
 }
