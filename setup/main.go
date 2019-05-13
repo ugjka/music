831db23b7573e301389d99797ca429d03cdb73b4
ugjka <esesmu@protonmail.com>
2019-05-13 23:53:05 +0300

    update builder

--- setup/main.go
+++ setup/main.go
@@ -7,10 +7,16 @@ import (
 )
 
 func main() {
-	_, err := exec.LookPath("bower")
+	_, err := exec.LookPath("npm")
 	if err != nil {
-		fmt.Println("FATAL ERROR: could not find Bower\n" +
-			"SEE: https://bower.io/ for installation instructions...")
+		fmt.Println("FATAL ERROR: could not find NPM\n" +
+			"SEE: https://nodejs.org/ for installation instructions...")
+		return
+	}
+	_, err = exec.LookPath("polymer")
+	if err != nil {
+		fmt.Println("FATAL ERROR: could not find Polymer CLI\n" +
+			"SEE: https://polymer-library.polymer-project.org/3.0/docs/tools/polymer-cli for installation instructions...")
 		return
 	}
 	err = os.Chdir("./public")
@@ -19,10 +25,27 @@ func main() {
 		return
 	}
 	fmt.Println("*** INSTALLING WEBCOMPONENTS ***")
-	cmd := exec.Command("bower", "install", "--config.interactive=false", "-f")
+	cmd := exec.Command("npm", "install")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
 	err = cmd.Run()
 	if err != nil {
 		fmt.Printf("FATAL ERROR: could not fetch webcomponents: %v", err)
+		return
+	}
+	fmt.Println("*** SUCCESS ***")
+	err = os.Chdir("../")
+	if err != nil {
+		fmt.Printf("FATAL ERROR: could not descend into main folder: %v\n", err)
+		return
+	}
+	fmt.Println("*** BUILDING POLYMER SOURCES ***")
+	cmd = exec.Command("polymer", "build")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err = cmd.Run()
+	if err != nil {
+		fmt.Printf("FATAL ERROR: could not build Polymer sources: %v", err)
 		return
 	}
 	fmt.Println("*** SUCCESS ***")
