4f8df50cef924dd5b8e920343ad0a1e705abb96b
ugjka <esesmu@protonmail.com>
2019-05-13 22:07:06 +0300

    proper polymer

---
+++ public/shell.js
@@ -0,0 +1,16 @@
+/*jshint esversion: 6 */
+
+import '@polymer/polymer/polymer-legacy.js';
+import '@polymer/iron-flex-layout/iron-flex-layout.js';
+import '@polymer/font-roboto/roboto.js';
+import '@polymer/iron-icons/iron-icons.js';
+import '@polymer/iron-icon/iron-icon.js';
+import '@polymer/paper-icon-button/paper-icon-button.js';
+import '@polymer/app-layout/app-drawer/app-drawer.js';
+import '@polymer/app-layout/app-scroll-effects/app-scroll-effects.js';
+import '@polymer/app-layout/app-header/app-header.js';
+import '@polymer/app-layout/app-toolbar/app-toolbar.js';
+import '@polymer/paper-button/paper-button.js';
+import '@polymer/iron-icons/av-icons.js';
+import '@polymer/paper-dialog/paper-dialog.js';
+import '@polymer/paper-input/paper-input.js';
