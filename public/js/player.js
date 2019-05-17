3f1a872e79b33612237f4c8305d54737835413cb
ugjka <esesmu@protonmail.com>
2019-05-17 12:32:22 +0300

    print album when track is not present

--- public/js/player.js
+++ public/js/player.js
@@ -59,10 +59,10 @@ const music = {
     let playlist = [""];
     for (let i = 0; i < this.playlist.length; i++) {
       let albumPrint = "";
-      if ((this.playlist[i].Track != 0) && (this.playlist[i].Album != "")) {
+      if (this.playlist[i].Track != 0) {
         albumPrint = `<td class='album'>[${this.playlist[i].Track}] ${this.playlist[i].Album}</td>`;
       } else {
-        albumPrint = "<td class='album'></td>";
+        albumPrint = `<td class='album'>${this.playlist[i].Album}</td>`;
       }
       playlist.push(
         `<tr id='sound${i}' class='song' index='${i}'>`,
